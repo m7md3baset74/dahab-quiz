5bd59f9167d62b89d110878e931f08f3b63b64e1
m7md3baset74 <mohamedabdelbasset265@gmail.com>
2026-04-01 22:22:18 +0200

    Done

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
       </div>
 
       {/* 🌑 Overlay (عشان النص يبان) */}
-      <div className="absolute inset-0 backdrop-blur-custom bg-black/50 " />
+      <div className="absolute inset-0 backdrop-blur-custom bg-black/40 " />
 
       {/* ✨ Glow Effect */}
       {/* <div className="absolute w-[300px] h-[300px] bg-purple-600 opacity-30 blur-[120px] rounded-full top-20 left-1/2 -translate-x-1/2" /> */}
